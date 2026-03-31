47a26e13f847c665b963ac5e580c7d5b0037aea5
tsora1603 <neiva.cesar07@proton.me>
2026-03-31 09:42:45 -0300

    Update ssh keys install script

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -2,6 +2,10 @@
 set -e
 
 mkdir -p ~/.ssh
-cp -r /mnt/extrastuff2/.ssh-backup $HOME/.ssh
+cp -r /mnt/extrastuff2/.ssh-backup/. ~/.ssh/
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/aur ~/.ssh/github
+chmod 644 ~/.ssh/aur.pub ~/.ssh/github.pub
 
 echo "ssh keys in place!"
