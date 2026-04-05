8dd96f43faa92a6895e33236caa4afcd06ce6063
tsora1603 <neiva.cesar07@proton.me>
2026-04-04 22:33:41 -0300

    Update yazi configs accordingly to new plugins

--- dots/.config/yazi/init.lua
+++ dots/.config/yazi/init.lua
@@ -1,3 +1,59 @@
+require("git"):setup {
+	-- Order of status signs showing in the linemode
+	order = 1500,
+}
+
+require("folder-rules"):setup()
+
+require("projects"):setup({
+    event = {
+        save = {
+            enable = true,
+            name = "project-saved",
+        },
+        load = {
+            enable = true,
+            name = "project-loaded",
+        },
+        delete = {
+            enable = true,
+            name = "project-deleted",
+        },
+        delete_all = {
+            enable = true,
+            name = "project-deleted-all",
+        },
+        merge = {
+            enable = true,
+            name = "project-merged",
+        },
+    },
+    save = {
+        method = "yazi", -- yazi | lua
+        yazi_load_event = "@projects-load", -- event name when loading projects in `yazi` method
+        lua_save_path = "", -- path of saved file in `lua` method, comment out or assign explicitly
+                            -- default value:
+                            -- windows: "%APPDATA%/yazi/state/projects.json"
+                            -- unix: "~/.local/state/yazi/projects.json"
+    },
+    last = {
+        update_after_save = true,
+        update_after_load = true,
+        update_before_quit = false,
+        load_after_start = false,
+    },
+    merge = {
+        event = "projects-merge",
+        quit_after_merge = false,
+    },
+    notify = {
+        enable = true,
+        title = "Projects",
+        timeout = 3,
+        level = "info",
+    },
+})
+
 Status:children_add(function(self)
 	local h = self._current.hovered
 	if h and h.link_to then
@@ -6,8 +62,6 @@ Status:children_add(function(self)
 		return ""
 	end
 end, 3300, Status.LEFT)
-
-require("full-border"):setup()
 
 require("bunny"):setup({
   hops = {
@@ -37,15 +91,6 @@ require("bunny"):setup({
 
 require("recycle-bin"):setup()
 
-require("autosession"):setup()
-
 require("simple-status"):setup()
 
-Status:children_add(function(self)
-	local h = self._current.hovered
-	if h and h.link_to then
-		return " -> " .. tostring(h.link_to)
-	else
-		return ""
-	end
-end, 3300, Status.LEFT)
+require("full-border"):setup()
