a94b96577a34b0e662642665aba40d282fad773f
tsora1603 <neiva.cesar07@proton.me>
2026-04-04 22:42:05 -0300

    Update fish config

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -4,8 +4,13 @@ if status is-interactive
 
     set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
+    # Start agent if not running
     if not test -S $SSH_AUTH_SOCK
         ssh-agent -a $SSH_AUTH_SOCK > /dev/null
+    end
+
+    # Add keys if not already loaded
+    if not ssh-add -l &>/dev/null
         ssh-add ~/.ssh/github ~/.ssh/aur
     end
 end
