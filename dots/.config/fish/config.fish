d47afda39a89cedccc5ea70d379152887eacd78b
tsora1603 <neiva.cesar07@proton.me>
2026-04-20 14:22:24 -0300

    Update fish config

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     set fish_greeting ""
     fastfetch
+
+    # Add keys if not already loaded
+    if not ssh-add -l &>/dev/null
+        ssh-add ~/.ssh/github ~/.ssh/aur
+    end
 end
 
 # Created by `pipx` on 2026-04-03 23:18:10
