f940b18b7c8894e3cb273ad6063d2e18d4233625
tsora1603 <neiva.cesar07@proton.me>
2026-05-09 17:41:35 -0300

    Update install.sh script
    Removed dolphin, hyprland, qt6ct-kde packages, and old aur repo git
    clone; Add gifski, qt6ct, niri, jq packages and cpu undervolt script and
    service.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ set -e
 
 # installing pacman packages
 sudo pacman -Syu \
-    dolphin \
     cachyos-gaming-meta \
     cachyos-gaming-applications \
     cliphist \
@@ -15,9 +14,9 @@ sudo pacman -Syu \
     firefoxpwa \
     greetd \
     grim \
-    hyprland \
     imagemagick \
     inkscape \
+    jq \
     kitty \
     lact \
     lazygit \
@@ -25,6 +24,7 @@ sudo pacman -Syu \
     mediainfo \
     mpd \
     mpd-mpris \
+    niri \
     nwg-look \
     onlyoffice \
     paru \
@@ -32,6 +32,7 @@ sudo pacman -Syu \
     playerctl \
     protonplus \
     python-lxml \
+    qt6ct \
     resvg \
     rmpc \
     scour \
@@ -54,6 +55,7 @@ sudo pacman -Syu \
 
 # installing AUR packages
 paru -S \
+    gifski \
     hydra-launcher-bin \
     hyprshutdown \
     millennium \
@@ -61,7 +63,6 @@ paru -S \
     otf-departure-mono-nerd \
     parui \
     python-pywalfox \
-    qt6ct-kde \
     vtracer \
     xdg-desktop-portal-termfilechooser-hunkyburrito-git \
     zoom
@@ -74,7 +75,6 @@ mkdir -p ~/Development
 cd ~/Development
 git clone https://github.com/tsora1603/pixora-icons.git
 git clone https://github.com/tsora1603/tsora-dots.git
-git clone https://aur.archlinux.org/pixora-icons-git.git
 
 # removing preinstalled fish config
 rm -rf ~/.config/fish
@@ -86,19 +86,38 @@ sudo rm -f /etc/greetd/config.toml
 sudo stow --target=/etc etc/
 git remote set-url origin git@github.com:tsora1603/tsora-dots.git
 
-# setting up pixora-icons git and installing
+# setting up pixora-icons repo and installing the theme
 cd ../pixora-icons
 git remote set-url origin git@github.com:tsora1603/pixora-icons.git
 ./install.sh
 
 # setting up fstab
 echo 'UUID=48D2B025D2B01960                     /mnt/extrastuff1   ntfs3   nofail,x-systemd.automount,noatime,uid=1000,gid=1000 0 0
-UUID=32FFB061010241EF                             /mnt/extrastuff2   ntfs3   nofail,x-systemd.automount,noatime,uid=1000,gid=1000 0 0' | sudo tee -a /etc/fstab
+UUID=32FFB061010241EF                      /mnt/extrastuff2   ntfs3   nofail,x-systemd.automount,noatime,uid=1000,gid=1000 0 0' | sudo tee -a /etc/fstab
+
+# setting up cpu undervolt
+cd ~/.local/bin
+git clone https://github.com/svenlange2/Ryzen-5800x3d-linux-undervolting.git
+mv Ryzen-5800x3d-linux-undervolting/ruv.py ./
+rm -rf Ryzen-5800x3d-linux-undervolting
+
+echo "ryzen_smu" | sudo tee /etc/modules-load.d/ryzen_smu.conf
+echo "[Unit]
+Description=PBO Curve Optimizer undervolt
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/python3 /home/tsora/.local/bin/ruv.py -c 8 -o -30
+
+[Install]
+WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/pbo-undervolt.service 
 
 # systemd services
 systemctl --user enable mpd
 systemctl --user enable mpd-mpris
 systemctl --user enable ssh-agent
+sudo systemctl enable pbo-undervolt.service
 sudo systemctl enable greetd
 
 # firefoxpwa runtime
